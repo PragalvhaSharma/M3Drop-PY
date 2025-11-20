c99bb247c703fa7584f310f8acff72bd6aa27ca4
Pragalvha Sharma <pshar223@uwo.ca>
2025-11-19 22:35:28 -0500

    fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="M3Drop",
-    version="0.3.2",
+    version="0.3.3",
     author="Tallulah Andrews, Anthony Son, Pragalvha Sharma",
     author_email="tandrew6@uwo.ca, json59@uwo.ca, pragalvhasharma@gmail.com",
     description="A Python implementation of the M3Drop single-cell RNA-seq analysis tool.",
