fd71c1f146607bfc21972b0b5d474f17b8bcc64e
Anthony-121 <anthonyson91@gmail.com>
2026-01-14 19:32:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="M3Drop",
-    version="0.4.38",  # Version bump
+    version="0.4.39",  # Version bump
     author="Tallulah Andrews",
     author_email="tandrew6@uwo.ca",
     description="A Python implementation of the M3Drop single-cell RNA-seq analysis tool.",
